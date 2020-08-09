ae2f6a780419e1c46be65977c70b8052da515f48
Aleksei <eastonsc6@yandex.ru>
2020-08-09 18:49:34 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+	/*new WOW({
+        animateClass: 'animate__animated'
+    }).init();*/
+    /*$('.header-arrows').on('click', function() {
+        $('html,body').animate({scrollTop:$('.surf').offset().top+"px"},{duration:1E3});
+    });*/
+
+});
